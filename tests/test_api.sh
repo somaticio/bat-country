a47b6bdb53862a5efb8ba353851e5f69b3a053ca
root <root@somatic>
2015-12-04 21:10:59 +0000

    added test suite

---
+++ tests/test_api.sh
@@ -0,0 +1,4 @@
+#docker build -t somatic/bat-country .
+docker run -d -p 5000:5000 somatic/bat-country  python /home/ubuntu/somaticagent/web.py -s
+sleep 1
+curl --fail -X POST -F i=@tests/slawek.jpg -F o=blah.jpg  http://127.0.0.1:5000/run
