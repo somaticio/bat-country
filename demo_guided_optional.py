1a88e248cace755adad1cc3dcaaca4d8806cf553
Jason Toy <jasontoy@gmail.com>
2015-12-14 12:25:03 -0600

    time

--- demo_guided_optional.py
+++ demo_guided_optional.py
@@ -8,7 +8,7 @@
 from batcountry import BatCountry
 from PIL import Image
 import numpy as np
-import argparse
+import argparse,time
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
